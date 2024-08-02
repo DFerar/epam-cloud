2e5f48b0021a15e7c5a0edc5828a801b97dee9bc
DFerar <mr.ferar@gmail.com>
2024-08-02 13:18:12 +0400

    wip

---
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -0,0 +1,38 @@
+package com.task02;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.model.RetentionSetting;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@LambdaHandler(
+        lambdaName = "hello_world",
+        roleName = "hello_world-role",
+        isPublishVersion = false,
+        logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+)
+public class HelloWorld implements RequestHandler<Map<String, Object>, Map<String, Object>> {
+
+    @Override
+    public Map<String, Object> handleRequest(Map<String, Object> request, Context context) {
+        System.out.println("Hello from lambda");
+
+        String path = (String) request.get("path");
+        String method = (String) request.get("httpMethod");
+
+        Map<String, Object> resultMap = new HashMap<>();
+        resultMap.put("headers", Map.of("Content-Type", "application/json"));
+
+        if ("/hello".equals(path) && "GET".equalsIgnoreCase(method)) {
+            resultMap.put("statusCode", 200);
+            resultMap.put("body", "{\"statusCode\": 200, \"message\": \"Hello from Lambda\"}");
+        } else {
+            resultMap.put("statusCode", 400);
+            resultMap.put("body", String.format("{\"statusCode\": 400, \"message\": \"Bad request syntax or unsupported method. Request path: %s. HTTP method: %s\"}", path, method));
+        }
+        return resultMap;
+    }
+}
