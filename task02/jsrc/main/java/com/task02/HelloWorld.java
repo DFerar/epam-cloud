adf863b269aa84291f8be60ede7a2a2bdaf29d21
DFerar <mr.ferar@gmail.com>
2024-08-04 14:36:31 +0400

    wip

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -3,6 +3,9 @@ package com.task02;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
+import com.syndicate.deployment.model.lambda.url.AuthType;
+import com.syndicate.deployment.model.lambda.url.InvokeMode;
 import com.syndicate.deployment.model.RetentionSetting;
 
 import java.util.HashMap;
@@ -13,6 +16,10 @@ import java.util.Map;
         roleName = "hello_world-role",
         isPublishVersion = false,
         logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+)
+@LambdaUrlConfig(
+        authType = AuthType.NONE,
+        invokeMode = InvokeMode.BUFFERED
 )
 public class HelloWorld implements RequestHandler<Map<String, Object>, Map<String, Object>> {
 
