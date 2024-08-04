fe30b3a9de6d75f3c6faaa32763d994749976231
DFerar <mr.ferar@gmail.com>
2024-08-04 16:56:09 +0400

    wip

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -22,22 +22,27 @@ import java.util.Map;
         authType = AuthType.NONE,
         invokeMode = InvokeMode.BUFFERED
 )
-public class HelloWorld implements RequestHandler<APIGatewayV2HTTPEvent, Map<String, Object>> {
-
-    @Override
-    public Map<String, Object> handleRequest(APIGatewayV2HTTPEvent request, Context context) {
+public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
+    public Map<String, Object> handleRequest(Object request, Context context) {
         System.out.println("Hello from lambda");
         Map<String, Object> resultMap = new HashMap<String, Object>();
-        String path = request.getRequestContext().getHttp().getPath();
-        String method = request.getRequestContext().getHttp().getMethod();
-        if ("/hello".equals(path) && "GET".equalsIgnoreCase(method)) {
-            resultMap.put("statusCode", 200);
-            resultMap.put("message", "{\"statusCode\": 200, \"message\": \"Hello from Lambda\"}");
-        } else {
-            resultMap.put("statusCode", 400);
-            resultMap.put("message", String.format("{\"statusCode\": 400, \"message\": \"Bad request syntax or unsupported method. Request path: %s. HTTP method: %s\"}", path, method));
-        }
+        resultMap.put("statusCode", 200);
+        resultMap.put("message", "Hello from Lambda");
         return resultMap;
     }
 
+//    @Override
+//    public Map<String, Object> handleRequest(APIGatewayV2HTTPEvent request, Context context) {
+//        System.out.println("Hello from lambda");
+//        Map<String, Object> resultMap = new HashMap<String, Object>();
+//        String path = request.getRequestContext().getHttp().getPath();
+//        String method = request.getRequestContext().getHttp().getMethod();
+//        if ("/hello".equals(path) && "GET".equalsIgnoreCase(method)) {
+//            resultMap.put("statusCode", 200);
+//            resultMap.put("message", "{\"statusCode\": 200, \"message\": \"Hello from Lambda\"}");
+//        } else {
+//            resultMap.put("statusCode", 400);
+//            resultMap.put("message", String.format("{\"statusCode\": 400, \"message\": \"Bad request syntax or unsupported method. Request path: %s. HTTP method: %s\"}", path, method));
+//        }
+//        return resultMap;
 }
