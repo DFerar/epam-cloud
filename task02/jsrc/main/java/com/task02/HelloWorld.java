2ab1cd261572800aed13472a4456ff251026786a
DFerar <mr.ferar@gmail.com>
2024-08-04 19:26:30 +0400

    wip

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -2,12 +2,13 @@ package com.task02;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
+import com.syndicate.deployment.model.RetentionSetting;
 import com.syndicate.deployment.model.lambda.url.AuthType;
 import com.syndicate.deployment.model.lambda.url.InvokeMode;
-import com.syndicate.deployment.model.RetentionSetting;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -22,13 +23,26 @@ import java.util.Map;
         authType = AuthType.NONE,
         invokeMode = InvokeMode.BUFFERED
 )
-public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
-    public Map<String, Object> handleRequest(Object request, Context context) {
-        System.out.println("Hello from lambda");
-        Map<String, Object> resultMap = new HashMap<String, Object>();
-        resultMap.put("statusCode", 200);
-        resultMap.put("message", "Hello from Lambda");
-        return resultMap;
+public class HelloWorld implements RequestHandler<APIGatewayV2HTTPEvent , APIGatewayV2HTTPResponse>{
+    private final Map<String, String> responseHeaders = Map.of("Content-Type", "application/json");
+
+    @Override
+    public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent request, Context context) {
+        String path = request.getRequestContext().getHttp().getPath();
+        String method = request.getRequestContext().getHttp().getMethod();
+        if (path.equals("/hello")) {
+            return APIGatewayV2HTTPResponse.builder()
+                    .withStatusCode(200)
+                    .withHeaders(responseHeaders)
+                    .withBody("{\"statusCode\": 200, \"message\": \"Hello from Lambda\"}")
+                    .build();
+        } else {
+            return APIGatewayV2HTTPResponse.builder()
+                    .withStatusCode(400)
+                    .withHeaders(responseHeaders)
+                    .withBody("{\"statusCode\": 400, \"message\": \"Bad request syntax or unsupported method. Request path: " + path + ". HTTP method: " + method + "\"}")
+                    .build();
+        }
     }
 
 //    @Override
@@ -46,3 +60,4 @@ public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
 //        }
 //        return resultMap;
 }
+
