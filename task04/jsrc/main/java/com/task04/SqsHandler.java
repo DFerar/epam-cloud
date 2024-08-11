e0b11b6728ad320843ce67248a283afd4387a598
DFerar <mr.ferar@gmail.com>
2024-08-11 19:10:18 +0400

    wip

--- task04/jsrc/main/java/com/task04/SqsHandler.java
+++ task04/jsrc/main/java/com/task04/SqsHandler.java
@@ -3,14 +3,9 @@ package com.task04;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent;
-import com.syndicate.deployment.annotations.events.SnsEvents;
-import com.syndicate.deployment.annotations.events.SqsEvents;
 import com.syndicate.deployment.annotations.events.SqsTriggerEventSource;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.model.RetentionSetting;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @LambdaHandler(
     lambdaName = "sqs_handler",
@@ -20,7 +15,8 @@ import java.util.Map;
 )
 @SqsTriggerEventSource(
 	targetQueue = "async_queue",
-	batchSize = 10)
+	batchSize = 10
+)
 public class SqsHandler implements RequestHandler<SQSEvent, String> {
 	@Override
 	public String handleRequest(SQSEvent event, Context context) {
