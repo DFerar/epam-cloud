4150e6617764b04a8cf20a63ff08e8c0d292a74f
DFerar <mr.ferar@gmail.com>
2024-08-12 15:19:36 +0400

    wip

--- task05/jsrc/main/java/com/task05/ApiHandler.java
+++ task05/jsrc/main/java/com/task05/ApiHandler.java
@@ -1,80 +1,55 @@
 package com.task05;
 
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.amazonaws.services.dynamodbv2.document.Item;
 import com.amazonaws.services.dynamodbv2.document.Table;
-import com.amazonaws.services.dynamodbv2.model.AttributeValue;
-import com.amazonaws.services.dynamodbv2.model.PutItemRequest;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.syndicate.deployment.annotations.environment.EnvironmentVariable;
 import com.syndicate.deployment.annotations.environment.EnvironmentVariables;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.model.RetentionSetting;
 
-import java.awt.event.ItemEvent;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
 @LambdaHandler(
-    lambdaName = "api_handler",
-	roleName = "api_handler-role",
-	isPublishVersion = false,
-	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+        lambdaName = "api_handler",
+        roleName = "api_handler-role",
+        isPublishVersion = false,
+        logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
 @EnvironmentVariables(value = {
-		@EnvironmentVariable(key = "target_table", value = "${target_table}")
+        @EnvironmentVariable(key = "target_table", value = "${target_table}")
 })
-public class ApiHandler implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
+public class ApiHandler implements RequestHandler<Map<String, Object>, Map<String, Object>> {
     private final AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
     private final DynamoDB dynamoDb = new DynamoDB(client);
     private final String DYNAMODB_TABLE_NAME = System.getenv("target_table");
-    private final Table table = dynamoDb.getTable(DYNAMODB_TABLE_NAME);
-    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final Table table = dynamoDb.getTable(DYNAMODB_TABLE_NAME);;
 
-	public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent request, Context context) {
-        Map<String, Object> input;
-        try {
-            input = objectMapper.readValue(request.getBody(), Map.class);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
-        UUID id = UUID.randomUUID();
+    public Map<String, Object> handleRequest(Map<String, Object> input, Context context) {
+        String id = UUID.randomUUID().toString();
+        int principalId = (int) input.get("principalId");
         String createdAt = LocalDateTime.now().toString();
+        Map<String, String> content = (Map<String, String>) input.get("content");
 
-        Map<String, String> headers = new HashMap<>();
-        headers.put("Content-Type", "application/json");
+        Item item = new Item()
+                .withPrimaryKey("id", id)
+                .withInt("principalId", principalId)
+                .withString("createdAt", createdAt)
+                .withMap("body", content);
+        table.putItem(item);
 
-        EventOutput output = new EventOutput(id.toString(), (Integer) input.get("principalId"), createdAt,
-                (Map<String, String>) input.get("content"));
+        Map<String, Object> response = new HashMap<>();
+        response.put("statusCode", 201);
+        response.put("event", item);
 
-        Item putItemRequest = new Item()
-                .withPrimaryKey("id", output.getId())
-                .withInt("principalId", output.getPrincipalId())
-                .withString("createdAt", output.getCreatedAt())
-                .withMap("body", output.getBody());
-        table.putItem(putItemRequest);
-        String stringOutput;
-        try {
-            stringOutput = objectMapper.writeValueAsString(output);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
-        return APIGatewayV2HTTPResponse.builder()
-                .withStatusCode(201)
-                .withHeaders(headers)
-                .withBody(stringOutput)
-                .build();
+        return response;
     }
 }
